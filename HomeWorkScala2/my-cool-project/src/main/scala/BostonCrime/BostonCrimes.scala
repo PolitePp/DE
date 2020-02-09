522320f14e01facb7be7ca87983ca76d2d90e707
PolitePp <vladimirk96rus@gmail.com>
2020-02-09 14:51:31 +0500

    Change second homework (spark sql) and comments

--- HomeWorkScala2/my-cool-project/src/main/scala/BostonCrime/BostonCrimes.scala
+++ HomeWorkScala2/my-cool-project/src/main/scala/BostonCrime/BostonCrimes.scala
@@ -18,46 +18,62 @@ object BostonCrimes extends App {
 
   import spark.implicits._
 
+  // На входе путь до папки, где хранятся csv и куда будет писать паркет
   val csvFolder1 = args(0)
-  val csvFoler2 = args(1)
-  val parqPath = args(2)
 
+  // Прочитали источники данных
   val crime = spark.read.format("csv").option("header", "true").load(s"$csvFolder1/crime.csv")
-  val offence_codes = spark.read.format("csv").option("header", "true").load(s"$csvFoler2/offense_codes.csv")
+  val offence_codes = spark.read.format("csv").option("header", "true").load(s"$csvFolder1/offense_codes.csv").groupBy("CODE", "NAME").count()
 
+  // Закэшировали словарь с кодами
   val OffenceCodesBroadcast = broadcast(offence_codes)
 
-  val df = crime
-    .join(OffenceCodesBroadcast, crime("OFFENSE_CODE").cast("Long") === OffenceCodesBroadcast("CODE"))
-    .select(coalesce($"DISTRICT", lit("Не определен")).as("DISTRICT"), $"Lat", $"Long", substring_index(col("NAME"), " - ", 1).as("crime_type"), $"MONTH")
+  // Сделали представления на основе источников для работы со SparkSQL
 
-  val df_top = df
-    .groupBy("DISTRICT", "crime_type")
-    .count()
-    .as[Test]
-    .groupByKey(x => x.DISTRICT)
-    .flatMapGroups {
-      case (district, iter) => iter.toList.sortBy(x => -x.count).take(3)
-  }
-    .groupBy("DISTRICT")
-    .agg(concat_ws(", ", collect_list("crime_type")).as("crime_type"))
+  crime.createOrReplaceTempView("crime")
+  OffenceCodesBroadcast.createOrReplaceTempView("offence_codes")
 
-  val df_count_avg = df
-    .groupBy("DISTRICT")
-    .agg(avg("Lat").as("avg_lat"), avg("Long").as("avg_long"), count("DISTRICT").as("count_crime"))
+  // Получаем общее кол-во преступлений по району и среднюю долготу и ширину (для джоина используем оба поля + выбираем уникальные значения)
 
-  val df_count_by_month = df
-      .groupBy("DISTRICT", "MONTH")
-      .count()
+  val df_common = spark.sql(
+    "SELECT coalesce(district, 'Not defined') as district, count(*) as crimes_total, avg(Lat) as lat, avg(Long) as lng " +
+    "FROM crime c " +
+    "inner join " +
+    "(select distinct code, name from offence_codes) oc " +
+    "on cast(c.offense_code as bigint) = code " +
+    "and c.offense_description = oc.name " +
+    "group by coalesce(district, 'Not defined') ")
 
-  df_count_by_month.createOrReplaceTempView("df")
+  // Находим медиану кол-ва преступлений по месяцам в разрезе районов
 
-  val df_med = spark.sql("SELECT DISTRICT, percentile_approx(count, 0.5) as medium FROM df GROUP BY DISTRICT")
+  val df_by_month = spark.sql(
+    "SELECT district, percentile_approx(medium, 0.5) as crimes_monthly " +
+      "from (" +
+      "select coalesce(district, 'Not defined') as district, month, count(*) as medium " +
+      "from crime " +
+      "group by coalesce(district, 'Not defined'), month " +
+      ") s1 group by district"
+  )
 
-  df_top.alias("top")
-    .join(df_count_avg.alias("count_avg"), Seq("DISTRICT"))
-    .join(df_med.alias("med"), Seq("DISTRICT"))
+  // Находим топ3 типов преступлений в разрезе района
+
+  val df_by_crime_type = spark.sql(
+    "SELECT district, concat_ws(', ', collect_list(crime_type)) as frequent_crime_types " +
+      "from (" +
+      "SELECT coalesce(district, 'Not defined') as district, split(offense_description, ' - ')[0] as crime_type, count(*) as cn, " +
+      "row_number() over (partition by coalesce(district, 'Not defined') order by count(1) desc) as rn " +
+      "from crime " +
+      "group by coalesce(district, 'Not defined'), split(offense_description, ' - ')[0] " +
+      ") s1 where rn < 4 " +
+      "group by district"
+  )
+
+  // Соединяем результаты и записываем в паркет
+
+  df_common
+    .join(df_by_month, Seq("district"))
+    .join(df_by_crime_type, Seq("district"))
     .repartition(1)
     .write
-    .parquet(s"$parqPath/output.parquet")
+    .parquet(s"$csvFolder1/output.parquet")
 }
